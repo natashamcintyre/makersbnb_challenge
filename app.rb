cabb68a33e19a0ca0f6023b86ebe7cdf9a798b17
Will Dixon <dixon.will1@gmail.com>
2021-01-04 17:11:51 +0000

    next feature test

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require 'sinatra'
 
 class MakersBnb < Sinatra::Base
 
+  get '/' do 
+    erb :index
+  end
+
+  get '/spaces/new' do
+    erb :spaces
+  end
+
+  # get '/spaces/new' do
+  #   "Hello World"
+  # end
+
 end
--- spec/features/users_can_list_new_space_spec.rb
+++ spec/features/users_can_list_new_space_spec.rb
@@ -2,10 +2,11 @@ feature 'list a new space' do
   scenario 'user can set up a new space for others to rent' do
     visit '/'
     # NEEDS CHANGING WHEN USER SIGN IN IS IMPLEMENTED
-    expect(page).to have_button 'List New Space'
+    expect(page).to have_link 'List New Space'
 
-    click_button 'List New Space'
+    click_link 'List New Space'
 
-    expect(current_path).to eq '/spaces'
+    expect(current_path).to eq '/spaces/new'
+    expect(page).to_not have_content 'Sinatra'
   end
 end
