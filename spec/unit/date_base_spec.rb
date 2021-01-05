8cb8c56c5615ac2570626989e2d4376888837318
Benedict Lawrence <BTLawrence44@gmail.com>
2021-01-05 10:11:49 +0000

    datebase_test

--- spec/unit/date_base_spec.rb
+++ spec/unit/date_base_spec.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-describe 'date_connection' do  
+describe 'datebase_connection' do  
     it 'makes a connection to the data base' do 
         expect(ActiveRecord::Base.connection.current_database).to eq 'makersbnb_test' 
     end 
